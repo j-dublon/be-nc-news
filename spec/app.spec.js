15b0e2845c5d717bff232bad65acc9ba2e02e02e
j-dublon <j.johnson.dublon@gmail.com>
2020-05-04 11:51:11 +0100

    fixed test re change of article limit

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -458,7 +458,7 @@ describe("app", () => {
             .get("/api/articles")
             .expect(200)
             .then(({ body: { articles } }) => {
-              expect(articles.length).to.equal(10);
+              expect(articles.length).to.equal(9);
               articles.forEach((article) => {
                 expect(article).to.have.all.keys(
                   "author",
